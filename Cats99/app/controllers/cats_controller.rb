982487738a82ae4af25426965819cb8747399341
tylerthomasallen <tylerthomasallen@gmail.com>
2018-09-11 17:11:39 -0700

    Add #new, #edit, #create, and #show methods to Cat Controller. Create views for each

---
+++ Cats99/app/controllers/cats_controller.rb
@@ -0,0 +1,35 @@
+class CatsController < ApplicationController
+  COLOR = ['blue', 'red', 'white', 'black', 'yellow', 'orange']
+
+  def index
+    @cats = Cat.all
+    render :index
+  end
+
+  def show
+    @cat = Cat.find(params[:id])
+    render :show
+  end
+
+  def new
+    @colors = COLOR
+    @cat = Cat.new
+    render :new
+  end
+
+  def create
+    @cat = Cat.create(cat_params)
+    redirect_to cat_url(@cat)
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :color, :birth_date, :sex, :description)
+  end
+end
